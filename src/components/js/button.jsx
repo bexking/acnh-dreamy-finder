0d44ab5080ef3d3dd05649e9b0538ff12a5add70
Rebecca King <bexking@Rebeccas-MacBook-Pro-2.local>
2020-05-27 20:25:47 +1200

    Getting button component working, adding content to home page and a  button with route to next page. Adding some styling to home page.

--- src/components/js/button.jsx
+++ src/components/js/button.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import '../css/button.css';
 
+const STYLES = [
+    "btn--primary--solid",
+    "btn--previous--outline",
+    "btn--next--solid",
+]
+
+const SIZES = [
+    "btn--large",
+    "btn--medium",
+    "btn--small",
+]
+
 export const Button = ({
     children, 
     type, 
@@ -9,8 +21,16 @@ export const Button = ({
     buttonSize
 }) => {
 
+    const checkButtonStyle = STYLES.includes(buttonStyle) 
+        ? buttonStyle 
+        : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize)
+        ? buttonSize 
+        : SIZES[0];
+
     return (
-        <button onClick={onClick} type={type}>
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
             {children}
         </button>
     )
--- src/pages/js/home.js
+++ src/pages/js/home.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import '../css/home.css';
+import { Link } from "react-router-dom";
 import { Button } from '../../components/js/button';
+import hero from '../../assets/dreamies-hero.png';
 
 export default function home() {
     return (
-        <div>
+        <div className="content">
             <div className="intro">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <Button>Get Started</Button>
+                <p className="intro-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <Link to="/CurrentVillager" className="intro-item">
+                    <Button
+                        type="button"
+                        buttonStyle="btn--primary-solid"
+                        >Get Started
+                    </Button>
+                </Link>
             </div>
+            <img className="hero-image" src={hero} alt="Illustration of Audie, Raymond and Marina" />
         </div>
     )
 }
